125ed9970a87532f80636ca107f1dd797cbfe832
shahd <shahdmohamed25112005@gmail.com>
2026-07-07 23:59:07 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,11 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:api', 'user'])->group(function () {
     Route::get('/sprints/{sprint_id}/tasks', [TaskController::class, 'index']);
-
     Route::post('/sprints/{sprint_id}/tasks', [TaskController::class, 'store']);
-
+    Route::get('/sprints/{sprint_id}/tasks/{task_id}', [TaskController::class, 'show']);
+    Route::put('/sprints/{sprint_id}/tasks/{task_id}', [TaskController::class, 'update']);
     Route::delete('/tasks/{task_id}', [TaskController::class, 'destroy']);
 });
